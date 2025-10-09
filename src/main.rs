57b329be349119dd8a16a0184dfe5b9ffd1adb72
Peter Szilvasi <peti.szilvasi95@gmail.com>
2025-10-09 17:54:32 +0200

    Stop navigation the tab forward tab if the ALT key is pressed.

--- src/main.rs
+++ src/main.rs
@@ -134,13 +134,9 @@ impl App {
         let pomodoro_sub = self.pomodoro.subscription().map(Message::Pomodoro);
 
         let tab_sub = iced::keyboard::on_key_press(|key, modifiers| match key.as_ref() {
-            Key::Named(Named::Tab) => {
-                if modifiers.shift() {
-                    Some(Message::NavigateTabBackward)
-                } else {
-                    Some(Message::NavigateTabForward)
-                }
-            }
+            Key::Named(Named::Tab) if modifiers.shift() => Some(Message::NavigateTabBackward),
+            // It always returns `false` if it pressed with the `TAB` key. See the https://github.com/iced-rs/iced/issues/3073 issue
+            Key::Named(Named::Tab) if !modifiers.alt() => Some(Message::NavigateTabForward),
             _ => None,
         });
 
